49347fb0f769ddb0783eeb4ee2917c7003958d25
Matheus Parolo <matheusparolo.m@gmail.com>
2017-11-24 17:04:42 -0200

    Sem retangulos, só olhos

--- Matheus/script.js
+++ Matheus/script.js
@@ -12,16 +12,29 @@ function preload() {
 // chamada no inicio do programa
 function setup() {
     // cria o quadro, com dimensoes 900 x 400
-    createCanvas(900, 400);
+    createCanvas(screen.width, screen.height);
 }
 
 // chamada toda vez que o quadro for redesenhado
 // ou seja, a cada frame da animacao
 function draw() {
-    // pinta o fundo de preto
-    background(255,255,255);
 
-    rect(50,150,25,25);
-    ellipse(width/2, height/2, 150, 150);
+    // pinta o fundo de preto
+//    background(255,255,255);
+
+    // rect(50,150,25,25);
+    fill(255,0,0);
+    push();
+    rotate(random(0,360))
+    pop();
+    fill(random(0,255),random(0,255),random(0,255));
+    ellipse(mouseX, mouseY, random(0,width), random(0,height));
+    stroke(random(0,255),random(0,255),random(0,255));
+    line(mouseX, mouseY, random(0,width), random(0,height));
+    line(mouseX, mouseY, random(0,width), random(0,height));
+    line(mouseX, mouseY, random(0,width), random(0,height));
+    line(mouseX, mouseY, random(0,width), random(0,height));
+    line(mouseX, mouseY, random(0,width), random(0,height));
+    line(mouseX, mouseY, random(0,width), random(0,height));
 
 }
