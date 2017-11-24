57e80fdf8dbdbbb3de4b5a76ddb24887cbbfb5a6
unknown <JessePires>
2017-11-24 17:00:33 -0200

    criação de várias elipses aleatórias

--- Jessé/script.js
+++ Jessé/script.js
@@ -12,16 +12,23 @@ function preload() {
 // chamada no inicio do programa
 function setup() {
     // cria o quadro, com dimensoes 900 x 400
-    createCanvas(900, 400);
+    createCanvas(900, 700);
 }
 
 // chamada toda vez que o quadro for redesenhado
 // ou seja, a cada frame da animacao
 function draw() {
     // pinta o fundo de preto
-    background(255, 255, 255);
+    //background(255, 255, 255);
 
-    rect(50, 150, 25, 25);
+    //rect(50, 150, 25, 25);
 
-    ellipse(width / 2, height / 2, 150, 150);
+    //ellipse(width / 2, height / 2, 150, 150);
+
+    var x = random(10, 900);
+    var y = random(10, 900);
+
+    fill(random(0, 255), random(0, 255), random(0, 100));
+
+    ellipse(width / 2, height / 2, x, y);
 }
